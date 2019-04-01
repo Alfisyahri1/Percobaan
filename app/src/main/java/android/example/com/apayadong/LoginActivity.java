b1a8fe2a1bc6f8dd9250df186e56ea40808ff4e5
nigasatriokumara <nigakumara@gmail.com>
2019-04-02 03:44:21 +0700

    Login User

---
+++ app/src/main/java/android/example/com/apayadong/LoginActivity.java
@@ -0,0 +1,15 @@
+package android.example.com.apayadong;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("User Login Form");
+    }
+}
